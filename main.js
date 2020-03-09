c05418661675b99c1e33648153c9bbf893b7cb40
Jonah Senzel <jsenzel1@gmail.com>
2020-03-09 16:05:06 +0200

    added a bunch

--- main.js
+++ main.js
@@ -4,25 +4,76 @@ let locations = [];
 
 let dataToPush;
 
+let submitted = false;
+
+console.log("REF " + ref);
+let myRef = ref;
+
 document.getElementById("nextButton").addEventListener('click', nextMap);
 document.getElementById("submitButton").addEventListener('click', submitData);
 
-function nextMap() {
+//DATALOAD
 
-    document.getElementById("map").src = source2;
+objData = ref.on("value", gotData);
+
+function gotData(data) {
+
+    var dataVals = data.val();
+    var keys = Object.keys(dataVals);
+
+    for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+
+        var curObj = (dataVals[key]);
+        locations.push(curObj);
+    }
+    console.log(locations);
+}
+
+//
+
+function nextMap() {
+    console.log(locations.length);
+
+    let randomObj = locations[Math.floor((Math.random() * locations.length))];
+
+    console.log(randomObj);
+
+    let formattedAdr = encodeURIComponent(randomObj.location.trim());
+
+    console.log(formattedAdr);
+
+    document.getElementById("map").src =
+
+        "https://maps.google.com/maps?q=" +
+        formattedAdr +
+        "&t=k&z=13&ie=UTF8&iwloc=&output=embed";
+
+
+    document.getElementById("description").innerHTML = randomObj.description;
 
 }
 
 function submitData() {
 
+    let locVal = document.getElementById("inputLocation").value;
+    let descVal = document.getElementById("inputDescription").value;
 
+    if (locVal.length > 0 && descVal.length > 0 && !submitted) {
 
-    let mapObj = {
-        location: document.getElementById("inputLocation").value,
-        description: document.getElementById("inputDescription").value
+        let mapObj = {
+            location: locVal,
+            description: descVal
+        }
+
+        console.log(mapObj);
+        ref.push(mapObj);
+        submitted = true;
     }
 
-    console.log(mapObj);
+    else {
+        console.log("no string or already submitted")
+    }
 
 }
 
